2932d1b7a907efc9e84be9dd872a95deb4306017
sosotechnologies <cafanwi@sosotechnologies.com>
2022-12-29 21:22:30 -0500

    updates

--- tenant/postgres-soso/main.tf
+++ tenant/postgres-soso/main.tf
@@ -20,45 +20,32 @@ locals {
   }
 }
 
-################################################################################
 # RDS Module
-################################################################################
 
 module "db" {
   source = "../../"
 
   identifier = local.name
-
-  # All available versions: https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/CHAP_PostgreSQL.html#PostgreSQL.Concepts
   engine               = "postgres"
   engine_version       = "14"
   family               = "postgres14" # DB parameter group
   major_engine_version = "14"         # DB option group
   instance_class       = "db.t4g.large"
-
   allocated_storage     = 20
   max_allocated_storage = 100
-
-  # NOTE: Do NOT use 'user' as the value for 'username' as it throws:
-  # "Error creating DB Instance: InvalidParameterValue: MasterUsername
-  # user cannot be used as it is a reserved word used by the engine"
   db_name  = "completePostgresql"
   username = "complete_postgresql"
   port     = 5432
-
   multi_az               = true
   db_subnet_group_name   = module.vpc.database_subnet_group
   vpc_security_group_ids = [module.security_group.security_group_id]
-
   maintenance_window              = "Mon:00:00-Mon:03:00"
   backup_window                   = "03:00-06:00"
   enabled_cloudwatch_logs_exports = ["postgresql", "upgrade"]
   create_cloudwatch_log_group     = true
-
   backup_retention_period = 1
   skip_final_snapshot     = true
   deletion_protection     = false
-
   performance_insights_enabled          = true
   performance_insights_retention_period = 7
   create_monitoring_role                = true
@@ -95,8 +82,6 @@ module "db_default" {
 
   create_db_option_group    = false
   create_db_parameter_group = false
-
-  # All available versions: https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/CHAP_PostgreSQL.html#PostgreSQL.Concepts
   engine               = "postgres"
   engine_version       = "14"
   family               = "postgres14" # DB parameter group
@@ -104,10 +89,6 @@ module "db_default" {
   instance_class       = "db.t4g.large"
 
   allocated_storage = 20
-
-  # NOTE: Do NOT use 'user' as the value for 'username' as it throws:
-  # "Error creating DB Instance: InvalidParameterValue: MasterUsername
-  # user cannot be used as it is a reserved word used by the engine"
   db_name  = "completePostgresql"
   username = "complete_postgresql"
   port     = 5432
@@ -132,10 +113,7 @@ module "db_disabled" {
   create_db_option_group    = false
 }
 
-################################################################################
 # RDS Automated Backups Replication Module
-################################################################################
-
 provider "aws" {
   alias  = "region2"
   region = local.region2
@@ -170,9 +148,7 @@ module "db_automated_backups_replication" {
   }
 }
 
-################################################################################
 # Supporting Resources
-################################################################################
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
